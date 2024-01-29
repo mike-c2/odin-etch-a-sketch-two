67a047716def1265545b58abd2d147465d4e376d
Mike Costa <mcosta1@gmail.com>
2024-01-28 20:32:42 -0500

    Add drawBoxes function
    This fills up the draw-container with identical boxes.
    
    The event listener still needs to be added to this function.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const MIN_GRID_SIZE = 1;
 const MAX_GRID_SIZE = 100;
 
+function drawBoxes() {
+  const gridSizeNumbers = document.querySelectorAll('.grid-size');
+  const gridSize = +gridSizeNumbers[0].textContent;
+  
+  const drawContainer = document.getElementById('draw-container');
+  const drawContainerSize = drawContainer.clientWidth;
+
+  // Divide by 10 because 1rem === 10px
+  const boxSize = (drawContainerSize / gridSize / 10) + 'rem';
+  const numberOfBoxes = gridSize * gridSize;
+
+  for(let i = 0; i < numberOfBoxes; i++) {
+    const newBox = document.createElement('div');
+    newBox.style.boxSizing = 'border-box';
+    newBox.style.border = 'none';
+    newBox.style.width = boxSize;
+    newBox.style.height = boxSize;
+    newBox.style.backgroundColor = 'black';
+    newBox.style.opacity = 0;
+
+    drawContainer.appendChild(newBox);
+  }
+}
+
 function setGridSize() {
   const gridSizeNumbers = document.querySelectorAll('.grid-size');
   const newGridSize = getNewGridSize();
@@ -20,3 +44,5 @@ function getNewGridSize() {
 
   return newGridSize;
 }
+
+drawBoxes();
